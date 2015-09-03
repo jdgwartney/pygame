8f95be877ef322df17ec6fe658637a684de71853
jdgwartney <davidg@boundary.com>
2015-09-02 20:00:37 -0700

    add files

---
+++ hello.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import pygame
+import sys
+
+from pygame.locals import *
+
+pygame.init()
+
+DISPLAYSURF = pygame.display.set_mode((400,300))
+
+pygame.display.set_caption('Hello World')
+
+while True:
+    for event in pygame.event.get():
+        print("event.type: " + str(event.type))
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
